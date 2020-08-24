8b8257b0ac9e834326004d2398139e1d320b7939
Ameen Mustafa <ameen@artemissoftware.net>
2020-08-24 12:00:33 -0600

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const geocode = require('./utils/geocode')
 console.log(__dirname)
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicDirectoryPath = path.join(path.join(__dirname, '../public'))
@@ -98,7 +99,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
 
